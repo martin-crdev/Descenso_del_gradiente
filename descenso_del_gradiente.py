a537bd4a629861b7786e8fc06d838c2bb7f9b4e5
Martin Cruz <martin_crdev@outloock.com>
2021-06-20 14:42:51 -0500

    Se grafico funcion en 3 dimensiones y mapa de nivel en 2d

--- descenso_del_gradiente.py
+++ descenso_del_gradiente.py
@@ -2,8 +2,33 @@ from matplotlib import cm #Para manejar colores
 import numpy as np
 import matplotlib.pyplot as plt
 
+def f(x,y):
+    return x**2 + y**2
+
 def run():
-    pass
+    #Graficar nuestra funcion 
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    res = 100
+
+    x = np.linspace(-4, 4, num=res)
+    y = np.linspace(-4, 4, num=res)
+    X, Y = np.meshgrid(x, y)
+
+    Z = f(X, Y)
+
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.cool)
+
+    plt.show()
+
+    #Curvas de nivel
+    level_map = np.linspace(np.min(Z), np.max(Z), res)
+    plt.contourf(X, Y, Z, levels=level_map, cmap=cm.cool)
+    plt.colorbar()
+    plt.title("Descenso del gradiente")
+
+    plt.show()
+
 
 if __name__ == "__main__":
     run()
